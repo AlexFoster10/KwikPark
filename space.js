075942d2c717c215ee3e1bf54509c1d3b0577872
AlexFoster10 <wookie471@outlook.com>
2024-03-17 21:31:42 +0000

    created space class

---
+++ space.js
@@ -0,0 +1,13 @@
+class Space{
+    #booked = false;
+    #area = 0; //idea is 20 spaces per area
+    #id = 0; //up to 20
+
+
+    constructor(area,id){
+        this.#area = area;
+        this.id = id;
+        this.#booked = false
+
+    }
+} 
